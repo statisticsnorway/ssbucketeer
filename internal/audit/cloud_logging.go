3cbcbbdf879e9473a4347a9a57436f33a8b1ef8e
nicolst <35232838+nicolst@users.noreply.github.com>
2024-11-08 11:51:37 +0100

    feat: read audit sink config from env vars

--- internal/audit/cloud_logging.go
+++ internal/audit/cloud_logging.go
@@ -1,11 +1,35 @@
 package audit
 
-import "cloud.google.com/go/logging"
+import (
+	"context"
+	"fmt"
+
+	"cloud.google.com/go/logging"
+)
 
 var _ Sink = (*CloudLogging)(nil)
 
 type CloudLogging struct {
 	Logger *logging.Logger
+}
+
+func NewCloudLoggingSink(ctx context.Context, config map[string]string) (*CloudLogging, error) {
+	projectId, ok := config["projectId"]
+	if !ok {
+		return nil, fmt.Errorf("missing `projectId` in config: %v", config)
+	}
+
+	logName, ok := config["logName"]
+	if !ok {
+		logName = "ssbucketeer"
+	}
+
+	client, err := logging.NewClient(ctx, fmt.Sprintf("projects/%s", projectId))
+	if err != nil {
+		return nil, err
+	}
+
+	return &CloudLogging{Logger: client.Logger(logName)}, nil
 }
 
 func (r *CloudLogging) Record(p Payload) error {
